ed4c4acd9f29a3e45f8ccd87e0de594323952f31
xiso <ceo@amuz.co.kr>
2024-06-28 17:06:06 +0900

    - 언어팩 추가

--- src/resources/views/fail.blade.php
+++ src/resources/views/fail.blade.php
@@ -1,5 +1,5 @@
 @extends('deep-link::layout')
 @section('content')
-    <h2>만료된 링크</h2>
-    <p>이 링크는 유효하지 않습니다.</p>
+    <h2>{{ __('Expired Link') }}</h2>
+    <p>{{ __('This link is no longer valid.') }}</p>
 @endsection
