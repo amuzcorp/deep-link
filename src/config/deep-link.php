390f2163d0844e41b586e22d1eb0bc85f35f59e9
jisoo <jisoo@amuz.co.kr>
2026-06-26 13:40:13 +0900

    환경별 딥링크 앱 대상 설정 추가

--- src/config/deep-link.php
+++ src/config/deep-link.php
@@ -1,24 +1,29 @@
 <?php
 
+$appDomain = env('APP_DOMAIN') ?: parse_url(env('APP_URL', ''), PHP_URL_HOST) ?: 'lgscdev.lge.com';
+$appDomain = parse_url($appDomain, PHP_URL_HOST) ?: $appDomain;
+$appDomain = trim($appDomain, '/');
+$isDevelopmentDomain = $appDomain === 'lgscdev.lge.com';
+
 return [
     'prefix' => 'dl',
     'pages' => [
         'run' => 'deep-link::run',
-        'fail' => 'deep-link::fail'
+        'fail' => 'deep-link::fail',
     ],
     'app' => [
         'ios' => [
-            'scheme' => 'lgsc',
+            'scheme' => env('DEEPLINK_IOS_SCHEME', $isDevelopmentDomain ? 'lgscdev' : 'lgsc'),
             'install_route' => 'application-install.ios',
-            'bundle' => 'com.lge.smartcheck',
+            'bundle' => env('DEEPLINK_IOS_BUNDLE', $isDevelopmentDomain ? 'com.lge.smartcheck.dev' : 'com.lge.smartcheck'),
         ],
         'android' => [
             'install_route' => 'application-install.aos',
-            'scheme' => 'lgsc.lge.com',
-            'package' => 'com.lge.smartcheck',
+            'scheme' => env('DEEPLINK_ANDROID_HOST', $appDomain),
+            'package' => env('DEEPLINK_ANDROID_PACKAGE', $isDevelopmentDomain ? 'com.lge.smartcheck.dev' : 'com.lge.smartcheck'),
         ],
         'default' => [
-            'install_route' => 'application-install.index'
-        ]
-    ]
+            'install_route' => 'application-install.index',
+        ],
+    ],
 ];
